9241fea9ed22390e9e5bd49993830e6ca44844ba
lncadrnn <lanceadrn.acal@gmail.com>
2025-11-29 16:48:29 +0800

    feat: added information in the about us scene

---
+++ src/main/java/com/example/typejam/AboutUsController.java
@@ -0,0 +1,23 @@
+package com.example.typejam;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+
+public class AboutUsController {
+
+    @FXML
+    private Button back_btn;
+
+    @FXML
+    private void onBack(ActionEvent event) {
+        try {
+            NavigationHelper.navigateBack(event);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Error navigating back: " + e.getMessage());
+        }
+    }
+}
